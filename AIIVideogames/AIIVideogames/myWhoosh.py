51e60f8d64e38c486fd6e17954269b56438afeb4
Manu <manuamores1212@gmail.com>
2018-01-17 12:47:20 +0100

    Whoosh completo y Sistema de recomendacion

--- AIIVideogames/AIIVideogames/myWhoosh.py
+++ AIIVideogames/AIIVideogames/myWhoosh.py
@@ -1,18 +1,15 @@
-import os, os.path
+# -*- coding: utf-8 -*-
+import os, os.path, sqlite3
 from whoosh.fields import *
 from whoosh.index import create_in, open_dir
-from whoosh.qparser import QueryParser
-from whoosh.filedb.filestore import FileStorage
 from whoosh.query import *
-from whoosh.qparser import FuzzyTermPlugin, MultifieldParser
-import sqlite3 as lite
-import sys
+from whoosh.qparser import QueryParser,FuzzyTermPlugin, MultifieldParser
 
 
 def inicia():
     if not os.path.exists("indiceJuego"):
         os.mkdir("indiceJuego")
-        esquemaJuego = Schema(titulo=KEYWORD(stored=True), descripcion=TEXT(stored=True), links=KEYWORD(stored=True),
+        esquemaJuego = Schema(titulo=KEYWORD(stored=True), descripcion=TEXT,
                               categorias=KEYWORD(stored=True), plataformas=KEYWORD(stored=True),
                               precio=NUMERIC(stored=True))
         indiceJuego = create_in("indiceJuego",esquemaJuego)
@@ -25,15 +22,15 @@ def inicia():
     return indiceJuego,parser
 
 
-def insertaJuego(titulo, descripcion, links, categorias, plataformas, precio, indiceJuego):
+def insertaJuego(titulo, descripcion, categorias, plataformas, precio, indiceJuego):
     writerJuego = indiceJuego.writer()
-    writerJuego.add_document(titulo=titulo, descripcion=descripcion, links=links, categorias=categorias,
+    writerJuego.add_document(titulo=titulo, descripcion=descripcion, categorias=categorias,
                              plataformas=plataformas, precio=precio)
     writerJuego.commit()
 
 
 def buscaPorTituloDesc(keyword, indiceJuego, parser):
-    keyword = keyword + "~4"
+    keyword = keyword + "~2"
     res = []
     with indiceJuego.searcher() as searcher:
         myquery = parser.parse(keyword)
@@ -94,7 +91,7 @@ def buscaPrecioRango(min, max, indiceJuego):
 
 
 def buscaPorTituloDescPrecioMayorQue(keyword, min, indiceJuego, parser):
-    keyword = keyword + "~4"
+    keyword = keyword + "~2"
     res = []
     with indiceJuego.searcher() as searcher:
         myquery = And([parser.parse(keyword), TermRange("precio", min, 99999999)])
@@ -104,7 +101,7 @@ def buscaPorTituloDescPrecioMayorQue(keyword, min, indiceJuego, parser):
 
 
 def buscaPorTituloDescPrecioMenorQue(keyword, max, indiceJuego, parser):
-    keyword = keyword + "~4"
+    keyword = keyword + "~2"
     res = []
     with indiceJuego.searcher() as searcher:
         myquery = And([parser.parse(keyword), TermRange("precio", -1, max)])
@@ -114,7 +111,7 @@ def buscaPorTituloDescPrecioMenorQue(keyword, max, indiceJuego, parser):
 
 
 def buscaPorTituloDescPrecioRango(keyword, min, max, indiceJuego, parser):
-    keyword = keyword + "~4"
+    keyword = keyword + "~2"
     res = []
     with indiceJuego.searcher() as searcher:
         myquery = And([parser.parse(keyword), TermRange("precio", min, max)])
@@ -127,16 +124,50 @@ def db(indiceJuego):
     con = None
 
     try:
-        con = lite.connect('vg.db')
+        con = sqlite3.connect('vg.db')
 
-        cur = con.cursor()
-        cur.execute('SELECT SQLITE_VERSION()')
+        cur1 = con.cursor()
+        cur1.execute("SELECT * FROM 'vg_game'")
+        juegos = cur1.fetchall()
+        cur2 = con.cursor()
+        cur2.execute("SELECT * FROM 'vg_page'")
+        paginas = cur2.fetchall()
+        cur3 = con.cursor()
+        cur3.execute("SELECT * FROM 'vg_category'")
+        categorias = cur3.fetchall()
 
-        data = cur.fetchone()
 
-        print "SQLite version: %s" % data
+        for juego in juegos:
+            cats=""
+            precio=0.0
+            plats=""
+            cur4 = con.cursor()
+            cur4.execute("SELECT * FROM 'vg_category_games' WHERE game_id={jid}".format(jid=juego.__getitem__(0)))
+            relacion_categoriasJuego = cur4.fetchall()
+            for rel in relacion_categoriasJuego:
+                #if juego.__getitem__(0)==rel.__getitem__(2):
+                if cats=="":
+                    cats=categorias[rel.__getitem__(1)-1].__getitem__(1)
+                else:
+                    cats+=","+categorias[rel.__getitem__(1) - 1].__getitem__(1)
+            cur5 = con.cursor()
+            cur5.execute("SELECT * FROM 'vg_gamepage' WHERE game_id={jid}".format(jid=juego.__getitem__(0)))
+            relacion_paginaJuego = cur5.fetchall()
+            for rel in relacion_paginaJuego:
+                #if juego.__getitem__(0) == rel.__getitem__(4):
+                if precio==0.0 or precio>rel.__getitem__(2):
+                    precio=rel.__getitem__(2)
+                if plats=="":
+                    plats=paginas[rel.__getitem__(3)-1].__getitem__(1)
+                else:
+                    plats =","+paginas[rel.__getitem__(3) - 1].__getitem__(1)
+            if cats=="":
+                cats=u"None"
+            if plats=="":
+                plats=u"None"
+            insertaJuego(juego.__getitem__(1),juego.__getitem__(2), cats, plats, precio, indiceJuego)
 
-    except lite.Error, e:
+    except sqlite3.Error, e:
 
         print "Error %s:" % e.args[0]
         sys.exit(1)
@@ -152,14 +183,14 @@ if __name__ == "__main__":
     indiceJuego,parser=inicia()
 
     # INSERTAR DATOS PRUEBA
-    #insertaJuego(u"titulo1", u"desc1", u"url1,url2",u"cat1,cat2,cat3",u"plat1,plat2,plat3",50,indiceJuego)
-    #insertaJuego(u"titulo2", u"desc2", u"url3,url4",u"cat2",u"plat3",20,indiceJuego)
+    #insertaJuego(u"titulo1", u"desc1",u"cat1,cat2,cat3",u"plat1,plat2,plat3",50,indiceJuego)
+    #insertaJuego(u"titulo2", u"desc2",u"cat2",u"plat3",20,indiceJuego)
 
     # INSERTAR DATOS DB
-    db(indiceJuego)
+    #db(indiceJuego)
 
     # TESTEO QUERIES
-    pro1 = buscaPorTituloDesc("desc",indiceJuego,parser)
+    pro1 = buscaPorTituloDesc("juego",indiceJuego,parser)
     print(pro1)
     print("-------------------------------")
     pro2 = buscaPorCategoria("cat2",indiceJuego)
